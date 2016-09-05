7cec2ba46b8a4e3c222ceaa9acea18644f4f21c7
Knut Rand <knutdrand@gmail.com>
2016-09-05 15:39:03 +0200

    La til linje

--- uke33.py
+++ uke33.py
@@ -7,10 +7,12 @@ def assertLinEq(a, b, c):
     print la.solve(a, b)
     assert np.allclose(la.solve(a, b), c)
 
-def assertRaises(a, b, c):
+
+def assertRaises(a, b):
     try:
         la.solve(a, b)
-    except:
+    except la.linalg.LinAlgError as e:
+        print e
         assert True
     else:
         assert False
@@ -19,15 +21,15 @@ def assertRaises(a, b, c):
 print("a")
 a = np.array([[1,1,2], [-1,-2,3], [3,-7,4]])
 b = np.array([8,1,10])
-#assertLinEq(a,b,(3,1,2))
+assertLinEq(a,b,(3,1,2))
 
 print("b")           
 a = [[2,-3, 0], [2,1,0], [3,2,0]]
 b = [-2,1,1]
+assertRaises(a,b)
 #print la.solve(a,b)
 
 print("c")
 a = np.array([[2,2,2], [-2,5,2], [8,1,4]])
 b = np.array([0,1,-1])
-assertLinEq(a,b,[-0.25,0,0.25])
-
+assertRaises(a,b)
